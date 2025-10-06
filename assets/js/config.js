1caa1dd1a38929cf2aeced2446e1654bef7f9612
Anders Duus <anderskink@gmail.com>
2025-10-06 20:40:08 +0200

    updated style

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,7 +3,7 @@
 ---
 // Configuration for waiver form
 window.LillebaeltConfig = {
-    WAIVER_SCRIPT_URL: '{{ site.env.WAIVER_SCRIPT_URL | default: "https://script.google.com/macros/s/AKfycbwVdaCC95Fwru8FJ0ywMczEE9a1WhMawicNFfv6qZLoHWy1tI8roOS7gNEPU1Dj7kZq/exec" }}',
+    WAIVER_SCRIPT_URL: '{{ site.env.WAIVER_SCRIPT_URL | default: "" }}',
     DEBUG_INFO: {
         environment: '{{ jekyll.environment }}',
         hasSecret: '{% if site.env.WAIVER_SCRIPT_URL and site.env.WAIVER_SCRIPT_URL != "" %}true{% else %}false{% endif %}',
